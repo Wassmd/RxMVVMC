8ee4e68f7c0ee0c0c0509c73f35f7b83648abd3d
Wasim <mohammed.wasimuddin@grandcentrix.net>
2019-09-05 21:48:52 +0200

    Add loader framework

---
+++ EndPointLoader.framework/Headers/EndPointLoader.h
@@ -0,0 +1,19 @@
+//
+//  EndPointLoader.h
+//  EndPointLoader
+//
+//  Created by Mohammed Wasimuddin on 04.09.19.
+//  Copyright © 2019 Wasim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EndPointLoader.
+FOUNDATION_EXPORT double EndPointLoaderVersionNumber;
+
+//! Project version string for EndPointLoader.
+FOUNDATION_EXPORT const unsigned char EndPointLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EndPointLoader/PublicHeader.h>
+
+
