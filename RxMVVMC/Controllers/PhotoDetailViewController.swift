f7549e781977d44f1306220f9e646f063f6657d8
Wasim <mohammed.wasimuddin@grandcentrix.net>
2019-09-07 21:47:23 +0200

    update gitignore

--- RxMVVMC/Controllers/PhotoDetailViewController.swift
+++ RxMVVMC/Controllers/PhotoDetailViewController.swift
@@ -35,6 +35,8 @@ class PhotoDetailViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
